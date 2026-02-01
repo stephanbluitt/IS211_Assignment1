96f32685b8ffc40c3155627955e4a26126d9b079
Stephan Bluitt <46945764+stephanbluitt@users.noreply.github.com>
2026-02-01 16:46:13 -0500

    Refactor list_divide function and improve testing
    Refactored the list_divide function and updated tests to use exceptions.

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,32 @@
+#assignment1
 
-def list_divide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
+class ListDivideException(Exception):
     pass
 
+
+def list_divide(numbers, divide=2):
+    count = 0
+    for num in numbers:
+        if num % divide == 0:
+            count += 1
+    return count
+
+
 def test_list_divide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
-if __name__ == "__main__":
-    testListDivide()
+    try:
+        if list_divide([1, 2, 3, 4, 5]) != 2:
+            raise ListDivideException
+        if list_divide([2, 4, 6, 8, 10]) != 5:
+            raise ListDivideException
+        if list_divide([30, 54, 63, 98, 100], divide=10) != 2:
+            raise ListDivideException
+        if list_divide([]) != 0:
+            raise ListDivideException
+        if list_divide([1, 2, 3, 4, 5], 1) != 5:
+            raise ListDivideException
+    except Exception:
+        raise ListDivideException
+
+
+#Test
+test_list_divide()
